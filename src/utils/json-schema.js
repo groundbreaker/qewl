d3b1e04018f719ae302764959abc5e27f4ab777f
Patrick Kilgore <patrick@groundbreaker.co>
2019-01-28 09:24:45 -0600

    refactoring typo fixed

--- src/utils/json-schema.js
+++ src/utils/json-schema.js
@@ -73,7 +73,7 @@ export const processProperties = (apiSchema, fields) => {
 
       const validTypes = { LIST: 1, INPUT_OBJECT: 1, SCALAR: 1, ENUM: 1 };
 
-      if (!(kind in validInputTypes)) {
+      if (!(kind in validTypes)) {
         kind = ofType.kind;
         name = ofType.name;
         ofType = ofType.ofType;
