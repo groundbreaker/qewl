a640cf071574e319f33f868ea5db16e4bf782e13
Isaah Forrest <isaah.forrest@gmail.com>
2019-03-08 12:49:33 -0600

    remove unnecessary test

--- test/query/index.test.js
+++
@@ -1,5 +0,0 @@
-import { gqlFetchDetail, gqlFetchList } from "../../src/common";
-
-describe("Qewl#query utility functions", () => {
-  it("Qewl#gqlFetchDetail returns expected query", () => {});
-});
