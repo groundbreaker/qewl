76503a6fc7d436e8af7d38f3c34ddc360022efc7
Patrick Kilgore <patrick@groundbreaker.co>
2019-11-19 17:36:18 -0600

    Adds result spooling code using react state rather than out-of-tree javascript state. (#84)
    * Adds result spooling code using react state rather than out-of-tree javascript state.
    
    * Removes unused nextToken const
    
    * Update src/query/index.js
    
    Co-Authored-By: Isaah Forrest <isaah@dropteched.com>
    
    * 1.26.0

--- src/query/index.js
+++ src/query/index.js
@@ -80,7 +80,6 @@ const decorateDetailBase = ({
   );
 };
 
-let pendingAutoFetch = false;
 const decorateListBase = ({
   dataKey,
   fetchPolicy,
@@ -106,34 +105,13 @@ const decorateListBase = ({
       props: ({ data, ownProps }) => {
         const fetchMore = data.fetchMore;
         const items = data[query] ? data[query].items : [];
-        const nextToken = data[query] ? data[query].nextToken : null;
-
-        if (
-          !pendingAutoFetch &&
-          !data.loading &&
-          items.length < 50 &&
-          nextToken
-        ) {
-          pendingAutoFetch = true;
-          fetchMore({
-            variables: { ...params, nextToken },
-            updateQuery: (previousResult, { fetchMoreResult }) => {
-              const next = fetchMoreResult[query];
-              const previous = previousResult[query];
-
-              const result = {
-                [query]: {
-                  ...next,
-                  items: [...next.items, ...previous.items]
-                }
-              };
-              pendingAutoFetch = false;
-              return result;
-            }
-          });
-        }
 
         return {
+          // These two props are used by the spooling functionality below.
+          _data: data,
+          stillFetching: true,
+          // The props below are essentially a flattened `data` prop that one
+          // would usually see come from react-apollo, with custom names.
           apolloInternalError: data.error,
           [dataKey ? `${dataKey}Loading` : "loading"]: data.loading,
           [dataKey ? `${dataKey}Refetch` : `refetch`]: params => {
@@ -169,10 +147,79 @@ const decorateListBase = ({
               }
             });
           },
+          // Flatten the data.items into data.  Note that items is guaranteed to
+          // be an array (but it might be empty)
           [dataKey || `data`]: items
         };
       }
-    })
+    }),
+    withPropsOnChange(
+      /**
+       * Spool out more results when
+       *   1) we have a _new_ nextToken and
+       *   2) there is no request currently in flight.
+       *
+       *  Right now, we use networkStatus===7 for this, because it means there
+       *  was _no error_, whereas props.loading is true for network status 8 (no
+       *  request in flight, but there was an error with the last network req)
+       */
+      (props, nextProps) => {
+        const data = props._data[query];
+        const nextData = nextProps._data[query];
+
+        const thisToken = data ? data.nextToken : "";
+        const nextToken = nextData ? nextData.nextToken : "";
+
+        return nextToken !== thisToken && nextProps._data.networkStatus === 7;
+      },
+      /**
+       * Do the result spooling.
+       * Injects a prop, `stilFetching: bool`, which when true indicates that
+       * some results have been returned from the list operation, but more are
+       * coming.
+       */
+      function qewlSpoolResults({ _data }) {
+        const queryResult = _data[query];
+        const nextToken = queryResult ? queryResult.nextToken : "";
+
+        /**
+         * When we are done spooling, `nextToken` goes from "something" -> "",
+         * which is a change, and so, `qewlSpoolResults` will still fire.
+         * But we do not want to do another refetch in that case, so we only
+         * call refetch when `nextToken` is truthy
+         */
+        if (nextToken) {
+          _data.fetchMore({
+            variables: { ...params, nextToken },
+            updateQuery: (previousResult, { fetchMoreResult }) => {
+              const next = fetchMoreResult[query];
+              const previous = previousResult[query];
+
+              // Spread the results from the fetchMore result into the prior
+              // result and return (add it to the cache, the app will react to
+              // the new data).
+              const result = {
+                [query]: {
+                  ...next,
+                  items: [...next.items, ...previous.items]
+                }
+              };
+
+              return result;
+            }
+          });
+          // Tell the UI that we are still handling nextTokens...
+          return { stillFetching: true };
+        }
+
+        /**
+         * This code is only reachable when nextToken === "", which signifies
+         * we are done spooling, and we can inform the UI that we are not still
+         * fetching.
+         */
+        return { stillFetching: false };
+      }
+    )
   );
 };
 
